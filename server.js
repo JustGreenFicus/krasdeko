39664916265e7048f0cdc7a21523c97516c4bc08
JustGreenFicus <Babushkin.kirill2014@gmail.com>
2026-04-22 14:02:06 +0300

    Update server.js

--- server.js
+++ server.js
@@ -72,3 +72,30 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Сервер Kras Deco запущен на порту ${PORT}`);
 });
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://JustGreenFicus:<OvEr8888888@>@krasdecobase.axx0rgf.mongodb.net/?appName=KrasDecoBase";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
