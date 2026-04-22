6c8fc93de94aedc28f8c8ddcaaeb133d6f0134e7
JustGreenFicus <Babushkin.kirill2014@gmail.com>
2026-04-22 18:08:24 +0300

    Update script.js

--- script.js
+++ script.js
@@ -129,3 +129,17 @@ function switchForm(type, element) {
     const underline = document.querySelector('.underline');
     underline.style.left = element.offsetLeft + 'px';
 }
+
+function togglePassword(inputId, icon) {
+    const passwordInput = document.getElementById(inputId);
+    
+    if (passwordInput.type === 'password') {
+        passwordInput.type = 'text';
+        icon.classList.remove('fa-eye');
+        icon.classList.add('fa-eye-slash');
+    } else {
+        passwordInput.type = 'password';
+        icon.classList.remove('fa-eye-slash');
+        icon.classList.add('fa-eye');
+    }
+}
